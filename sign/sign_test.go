f4ada42f39586c212516f29b641ede55c4fd86f3
solomonooo <hoshinight@gmail.com>
2015-05-29 16:40:10 +0800

    ver 1.0.1

--- sign/sign_test.go
+++ sign/sign_test.go
@@ -16,10 +16,10 @@ const APPID = 200941
 const SECRET_ID = "AKIDh51wIFHJ13Mbc5AWd37z6WmQwIdTghBu"
 const SECRET_KEY = "SU4Qn0GoK0YRNS97p0l5rAsxwxcN6Il3"
 
-func TestQc_app_sign(t *testing.T) {
+func TestAppSign(t *testing.T) {
 	var userid uint = 123456
 	var expire uint = 3600
-	sign, err := Qc_app_sign(APPID, SECRET_ID, SECRET_KEY, expire, userid)
+	sign, err := AppSign(APPID, SECRET_ID, SECRET_KEY, expire, userid)
 	if err != nil {
 		t.Errorf("gen sign failed, err = %s\n", err.Error())
 	} else {
@@ -27,10 +27,10 @@ func TestQc_app_sign(t *testing.T) {
 	}
 }
 
-func TestQc_app_sign_once(t *testing.T) {
+func TestAppSignOnce(t *testing.T) {
 	var userid uint = 123456
 	url := "http://web.image.myqcloud.com/2011541224/123456/442d8ddf-59a5-4dd4-b5f1-e38499fb33b4/orignal"
-	sign, err := Qc_app_sign_once(APPID, SECRET_ID, SECRET_KEY, userid, url)
+	sign, err := AppSignOnce(APPID, SECRET_ID, SECRET_KEY, userid, url)
 	if err != nil {
 		t.Errorf("gen sign failed, err = %s\n", err.Error())
 	} else {
